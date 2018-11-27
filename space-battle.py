08cec333dcb76670e6454f25bc8cdcf3acce65a6
Mateus Koppe <mateus.koppe@gmail.com>
2018-11-27 02:42:29 -0200

    Create projectile

--- space-battle.py
+++ space-battle.py
@@ -7,6 +7,8 @@ screen = {
     "height": 600,
 }
 
+projectiles = []
+
 speed = 5
 
 clock = pygame.time.Clock()
@@ -18,6 +20,8 @@ pygame.display.set_caption("Space battle")
 class Ship:
     width = 71
     height = 85
+    gunLastShot = 0
+    gunDelay = 25
 
     def __init__(self, x, y, image):
         self.y = 0
@@ -30,10 +34,12 @@ class Ship:
 class Player(Ship):
     def __init__(self):
         self.y = screen["height"] - self.height - 10
-        self.x = screen["width"] / 2 - self.width / 2
+        self.x = screen["width"] // 2 - self.width // 2
         self.image = pygame.image.load("assets/player-ship.png")
 
     def update(self):
+        self.gunLastShot += 1
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT]:
@@ -41,6 +47,38 @@ class Player(Ship):
 
         if keys[pygame.K_RIGHT]:
             self.x += speed
+
+        if keys[pygame.K_SPACE] and self.gunLastShot > self.gunDelay:
+            self.gunLastShot = 0
+            projectile = Projectile((self.x + self.width // 2, self.y), Projectile.UP)
+            projectiles.append(projectile)
+
+        self.render()
+
+class Projectile:
+    UP = "UP"
+    DOWN = "DOWN"
+    speed = 15
+
+    def __init__(self, position, direction):
+        self.direction = direction
+        self.x, self.y = position
+
+    def render(self):
+        pygame.draw.circle(window, (255, 255, 255), (self.x, self.y), 5)
+
+    def update(self):
+        if (self.direction == self.UP):
+            self.y -= self.speed
+        if (self.direction == self.DOWN):
+            self.y += self.speed
+
+        self.render()
+
+def projectiles_update():
+    for projectile in projectiles:
+        print(projectile.x)
+        projectile.update()
 
 run = True
 player = Player()
@@ -52,6 +90,7 @@ while run:
             run = False
 
     player.update()
+    projectiles_update()
     pygame.display.update()
     
 pygame.quit()
